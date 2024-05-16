9120d9173343dc3d5ecd4b2bdfeccb1f9499ce82
Ahnaf Hasan Shifat <sheikhahnafshifat@gmail.com>
2024-05-16 11:32:39 +0600

    Add verify token api call in auth.js and remove api.js file

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,6 @@
-import {jwtDecode} from 'jwt-decode';
-import Cookie from 'js-cookie';
-import api from './api';
+import { jwtDecode } from "jwt-decode";
+import Cookie from "js-cookie";
+import axios from "axios";
 
 export async function verifyToken(token, refreshToken) {
   if (!token) {
@@ -10,26 +10,34 @@ export async function verifyToken(token, refreshToken) {
   try {
     const decoded = jwtDecode(token);
     const currentTime = Date.now() / 1000;
-    if (decoded.exp < currentTime) {
 
+    if (decoded.exp < currentTime) {
       if (!refreshToken) {
         return { authorized: false };
       }
-      
-      const res = await api.post('/api/token/refresh/', { refresh: refreshToken });
-      
+
+      const res = await axios.post(
+        "http://localhost:8000/api/token/refresh/",
+        { refresh: refreshToken },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
       if (res.status === 200) {
         // Return new access token to update in the client
-        Cookie.set('ACCESS_TOKEN', res.data.access, { expires: 7 }); // Expires in 1 day
-        Cookie.set('REFRESH_TOKEN', res.data.refresh, { expires: 1 });
+        Cookie.set("ACCESS_TOKEN", res.data.access, { expires: 7 }); // Expires in 1 day
+        Cookie.set("REFRESH_TOKEN", res.data.refresh, { expires: 1 });
         return { authorized: true, newToken: res.data.access };
       }
       return { authorized: false };
     }
-
+    const res = await axios.get(
+      "http://localhost:8000/verify-token/",
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    // console.log(res);
     return { authorized: true };
   } catch (error) {
-    console.error('Token verification failed:', error);
+    console.error("Token verification failed:", error);
     return { authorized: false };
   }
 }
